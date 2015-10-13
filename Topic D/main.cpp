e06d83b3c1a3372bee19c90c981dbf971df935d0
Benjamin Dahl <ben.dahl@mail.com>
2015-10-13 15:32:51 -0700

    Initial Commit

---
+++ Topic D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Topic D
+//
+//  Created by user on 10/13/15.
+//  Copyright (c) 2015 Ben Dahl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
